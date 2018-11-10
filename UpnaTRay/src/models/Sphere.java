b6f0e148b7d6ca2b7c8ba3213dcd8948a2e0e52c
Ruben <rufopufo@gmail.com>
2018-11-10 16:52:27 +0100

    esfera constructors

--- UpnaTRay/src/models/Sphere.java
+++ UpnaTRay/src/models/Sphere.java
@@ -9,17 +9,24 @@ import tracer.Hit;
 import tracer.Ray;
 
 public class Sphere extends Object3D {
-
+    private Point3D C;
+    private float radio;
   private Sphere (final Point3D C, final float radio, final Color color, final Material material) {
     super(color, material);
+    this.C = C;
+    this.radio = radio;
   }    
 
   public Sphere (final Point3D C, final float radio, final Color color) {
     this(C, radio, color, null);
+    this.C = C;
+    this.radio = radio;
   }
   
   public Sphere (final Point3D C, final float radio, final Material material) {
     this(C, radio, null, material);
+    this.C = C;
+    this.radio = radio;
   }  
 
   @Override
